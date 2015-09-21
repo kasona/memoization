367a9da126cb72954992720961ae650c10c92221
kasi <kasionaga@gmail.com>
2015-09-21 12:26:42 -1000

    Updated

--- memoization.js
+++ memoization.js
@@ -5,21 +5,22 @@
 * does not have to redo all of the functions it did before to get to the next point.
 */
 
-//Add this to the function you want to keep a cache to
-FunctionNameToAddCache.prototype.memoize = function() {
-  // cache will store the results that have already been processed
-  // recFunctionAnswer is the result that you are looking for
-  var recFunctionAnswer = this, cache = {}; // cache stores in objects, if want to store strings will need to convert objects into string
-  //return a function, this will look for cache or return the last knowing cache so the function can start from there.
-  return function( arg ) {
-      // look to see if the arg is already stored into the cache
-      if (arg in cache) {
-        // if it finds it then return the result
-        return cache[arg];
-      } else {
-        // if the result is not stored in the cache then it will run the function again from the last stored cache result
-        // and add the result, self (arg), to the cache for future reference.
-        return cache[arg] = recFunctionAnswer( arg );
-      }
-    };
-};
+var elementByID = (function() {
+  var cache = {};
+
+  function name(obj) {
+    //find out if its already in the cache
+    if(obj in cache) {
+      console.log(obj, 'object  is in cache');
+      return cache[obj];
+
+    }else {
+      //add to cache
+      cache[obj] = document.getElementById(obj);
+      console.log(obj, 'is not in the cache');
+      return cache[obj];
+    }
+  }
+
+  return name;
+})() ;
